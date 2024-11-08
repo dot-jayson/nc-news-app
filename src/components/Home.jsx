a9752eabdbfeb4140253a89b24f71179726bb564
Jayson Chiu <jaysonchiu@Jaysons-MacBook-Air.local>
2024-11-08 11:24:20 +0000

    clean up, added username on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useContext } from "react";
+import { UserContext } from "../context/User";
 
 const Home = () => {
-  return <h1 className="text-2xl text-center">Welcome to NC News</h1>;
+  const { signedInUser } = useContext(UserContext);
+  return (
+    <h1 className="text-2xl text-center">Welcome to NC News {signedInUser}!</h1>
+  );
 };
 
 export default Home;
